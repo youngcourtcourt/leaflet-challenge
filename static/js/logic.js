7922c031a6556dbb0fcf72aa611e8bd9748b685e
youngcourtcourt <courtneymacdonald@Courtneys-iMac.local>
2021-05-06 13:34:26 -0700

    Got map to show up

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,3 +1,18 @@
+var myMap=L.map("map", {
+    center:[17.92, -66.92], 
+    zoom:4
+})
+
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 10,
+  zoomOffset: -1,
+  id: 'mapbox/streets-v11',
+  accessToken: API_KEY
+}).addTo(myMap);
+
+
 var startTime='2020-01-01'
 var endTime='2020-01-02'
 
@@ -16,6 +31,5 @@ d3.json(url).then(function(data){
     var place=features.properties.place
     var time=new Date(features.properties.time)
 
-    console.log(lat, long, depth, mag, place, time)
 }
 })
