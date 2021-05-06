8d01507dd8dd3d4c66cb86abc806748afd2eeaaa
youngcourtcourt <courtneymacdonald@Courtneys-iMac.local>
2021-05-06 14:59:26 -0700

    Added comments to code

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,10 @@
+// Create Map
 var myMap=L.map("map", {
     center:[17.92, -66.92], 
     zoom:3
 })
+
+// Create tile L=layer
 
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -12,11 +15,14 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
+// Define api call parameters and url
 
 var startTime='2020-01-01'
 var endTime='2020-01-02'
 
 var url=`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${startTime}&endtime=${endTime}`
+
+// Get colors depending on depth
 
 function getColor(depth){
     
@@ -35,19 +41,17 @@ function getColor(depth){
         return "#2a4858"
     }
 }
-
-
+// Create legend
 
 var legend = L.control({
     position: "bottomright"
   })
   
-  // When the layer control is added, insert a div with the class of "legend"
   legend.onAdd = function() {
     var div = L.DomUtil.create("div", "info legend");
     var grades=[10,30,50,70,90]
     labels=[]
-    // var labels=['#fafa6e','#9cdf7c','#4abd8c','#00968e','#106e7c','#2a4858']
+ 
     for (var i = 0; i < grades.length; i++) {
         div.innerHTML +=
             '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
@@ -55,11 +59,15 @@ var legend = L.control({
     }
     return div;
   };
-  // Add the info legend to the map
+
   legend.addTo(myMap);
+
+//   Call data
 
 
 d3.json(url).then(function(data){
+
+    // Loop through data, storing in variabls
 
     for (var i=0;i<data.features.length;i++){
     
@@ -71,6 +79,8 @@ d3.json(url).then(function(data){
     var mag=features.properties.mag
     var place=features.properties.place
     var time=new Date(features.properties.time)
+
+    // Create circles based on magnitude and bind popup
 
     var circles=L.circle([lat,long],{
         color:"none",
